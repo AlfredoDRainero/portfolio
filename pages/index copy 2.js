d2c7e5c6d7c809e2b46070b8d4dae35e0570330d
Alfredo D Rainero <alfredo.rainero@gmail.com>
2023-11-16 00:24:27 -0300

    Update: test3

--- pages/index copy 2.js
+++ pages/index copy 2.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React, { Suspense, useRef } from "react";
+import React, { Suspense } from "react";
 import { useState, useEffect } from "react";
 
 import { useThree } from "@react-three/fiber";
@@ -65,7 +65,72 @@ const MainContainer = styled.div`
   color: black;
 `;
 
+function CarShow() {
+  return (
+    <>
+      {/*<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 
+      <PerspectiveCamera makeDefault fov={50} position={[0, 10, 0]} />
+
+      <color args={[0.9, 0.9, 0.9]} attach="background" />
+
+      <spotLight
+        color={[1, 1, 1]}
+        intensity={40.5}
+        angle={10}
+        penumbra={0.5}
+        position={[1, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={40}
+        angle={10}
+        penumbra={0.5}
+        position={[-1, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={40}
+        angle={10}
+        penumbra={0.5}
+        position={[-3, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={40}
+        angle={10}
+        penumbra={0.5}
+        position={[3, 5, 4]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <EffectComposer>
+        <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+      {/*<Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3} // The bloom intensity.
+          width={300} // render width
+          height={300} // render height
+          kernelSize={5} // blur kernel size
+          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          offset={[0.0005, 0.0012]} // color offset
+        />
+      </EffectComposer>
+       <Ground />*/}
+    </>
+  );
+}
 
 export default function Home() {
   const { gridSize, ...gridConfig } = useControls({
@@ -81,46 +146,46 @@ export default function Home() {
     followCamera: false,
     infiniteGrid: true,
   });
-  const [hovered, hover] = useState(false)
-  const [hovered2, hover2] = useState(false)
-  const ref = useRef()
-  const ref2 = useRef()
   return (
     <>
       <Canvas shadows camera={{ position: [10, 12, 12], fov: 25 }}>
-      <spotLight
-        color={[1, 1, 1]}
-        intensity={40.5}
-        angle={10}
-        penumbra={0.5}
-        position={[1, 5, 0]}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-        <ambientLight intensity={0.5} color="#ffffff" />
-
         <color args={[0.9, 0.9, 0.9]} attach="background" />
 
         <group position={[0, -0.5, 0]}>
           <Center top></Center>
           <Center top position={[-2, 0, 2]}>
-          <mesh castShadow ref={ref} onPointerOver={(event) => hover(true)} onPointerOut={(event) => hover(false)} >
+            <mesh castShadow>
               <sphereGeometry args={[0.5, 32, 32]} />
-              <meshStandardMaterial color={hovered ? 'white' : 'cyan'} />
-                          </mesh>
+              {/*   <Html
+                occlude
+                distanceFactor={1.5}
+                position={[0, 0, 0.51]}
+                transform
+              >
+                <span>Size</span>
+
+                <a> probando</a>
+                <button> asdasd</button>
+  </Html>*/}
+            </mesh>
           </Center>
 
           <Center top position={[2.5, 0, 1]}>
-            <mesh castShadow rotation={[0, Math.PI / 4, 0]} ref={ref2} onPointerOver={(event) => hover2(true)} onPointerOut={(event) => hover2(false)}>
+            <mesh castShadow rotation={[0, Math.PI / 4, 0]}>
               <boxGeometry args={[0.7, 0.7, 0.7]} />
-              <meshStandardMaterial color={hovered2 ? 'white' : 'cyan'} />
             </mesh>
           </Center>
           <Shadows />
           <Grid position={[0, -0.01, 0]} args={gridSize} {...gridConfig} />
         </group>
         <OrbitControls makeDefault />
-        
+         {/*<Environment preset="city" />
+       <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+          <GizmoViewport
+            axisColors={["#9d4b4b", "#2f7f4f", "#3b5b9d"]}
+            labelColor="white"
+/>
+        </GizmoHelper>*/}
       </Canvas>
     </>
   );
